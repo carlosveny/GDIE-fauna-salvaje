5240cf290bf534d9e6b2b9d8a0618aa29fe354ab
Carlos Veny <carlosveny@yahoo.es>
2022-03-16 01:57:22 +0100

    prueba

--- deploy.php
+++ deploy.php
@@ -1,50 +1,98 @@
 <?php
-	/**
-	 * GIT DEPLOYMENT SCRIPT
-	 *
-	 * Used for automatically deploying websites via github or bitbucket, more deets here:
-	 *
-	 *		https://gist.github.com/1809044
-	 */
 
-	// The commands
-	$commands = array(
-		'echo $PWD',
-		'whoami',
-		'git pull',
-		'git status',
-		'git submodule sync',
-		'git submodule update',
-		'git submodule status',
-	);
+// Forked from https://gist.github.com/1809044
+// Available from https://gist.github.com/nichtich/5290675#file-deploy-php
 
-	// Run the commands for output
-	$output = '';
-	foreach($commands AS $command){
-		// Run it
-		$tmp = shell_exec($command);
-		// Output
-		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
-		$output .= htmlentities(trim($tmp)) . "\n";
-	}
+$TITLE   = 'Git Deployment Hamster';
+$VERSION = '0.11';
 
-	// Make it pretty for manual user access (and why not?)
-?>
+echo <<<EOT
 <!DOCTYPE HTML>
 <html lang="en-US">
 <head>
 	<meta charset="UTF-8">
-	<title>GIT DEPLOYMENT SCRIPT</title>
+	<title>$TITLE</title>
 </head>
 <body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
 <pre>
- .  ____  .    ____________________________
- |/      \|   |                            |
-[| <span style="color: #FF0000;">&hearts;    &hearts;</span> |]  | Git Deployment Script v0.1 |
- |___==___|  /              &copy; oodavid 2012 |
-              |____________________________|
+  o-o    $TITLE
+ /\\"/\   v$VERSION
+(`=*=') 
+ ^---^`-.
+EOT;
 
-<?php echo $output; ?>
+// Check whether client is allowed to trigger an update
+
+$allowed_ips = array(
+	'207.97.227.', '50.57.128.', '108.171.174.', '50.57.231.', '204.232.175.', '192.30.252.', '140.82.115.', // GitHub
+);
+
+$allowed = false;
+$headers = apache_request_headers();
+
+if (@$headers["X-Forwarded-For"]) {
+    $ips = explode(",",$headers["X-Forwarded-For"]);
+    $ip  = $ips[0];
+} else {
+    $ip = $_SERVER['REMOTE_ADDR'];
+}
+
+foreach ($allowed_ips as $allow) {
+    if (stripos($ip, $allow) !== false) {
+        $allowed = true;
+        break;
+    }
+}
+
+if (!$allowed) {
+	header('HTTP/1.1 403 Forbidden');
+    echo "<span style=\"color: #ff0000\">Sorry, no hamster - better convince your parents!</span>\n";
+    echo "Your IP: ".$ip;
+    echo "</pre>\n</body>\n</html>";
+    exit;
+}
+
+flush();
+
+// Actually run the update
+$gh_token = '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';
+
+$repo = 'https://github.com/carlosveny/GDIE-fauna-salvaje.git';
+
+$commands = array(
+    'export GITHUB_TOKEN='.$gh_token,
+    'git remote set-url origin https://'.$gh_token.':x-oauth-basic@github.com/'. $repo . '.git',
+	'git pull -u origin master',
+	// 'git status',
+	// 'git submodule sync',
+	// 'git submodule update',
+	// 'git submodule status',
+    // 'test -e /usr/share/update-notifier/notify-reboot-required && echo "system restart required"',
+);
+
+$output = "\n";
+
+$log = "####### ".date('Y-m-d H:i:s'). " #######\n";
+
+foreach($commands AS $command){
+    // Run it
+    $tmp = shell_exec("$command 2>&1");
+    // Output
+    if($command == "git pull -u origin master"){
+        $output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
+        $output .= htmlentities(trim($tmp)) . "\n";
+    }
+
+    $log  .= "\$ $command\n".trim($tmp)."\n";
+}
+
+$log .= "\n";
+
+file_put_contents ('deploy-log.txt',$log,FILE_APPEND);
+
+echo $output; 
+
+?>
 </pre>
 </body>
 </html>
