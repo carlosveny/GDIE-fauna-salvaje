fa0706e8bf74c7e8c0a05bbc6f65c241f19357e9
Carlos Veny <carlosveny@yahoo.es>
2022-03-16 21:53:16 +0100

    Script updated

--- .ignore/deploy.php
+++ .ignore/deploy.php
@@ -2,6 +2,4 @@
 shell_exec("rm -r repo");
 shell_exec("mkdir repo");
 shell_exec("git clone https://github.com/carlosveny/GDIE-fauna-salvaje.git /var/www/html/repo");
-
-//echo "<pre>$output</pre>";
 ?>
