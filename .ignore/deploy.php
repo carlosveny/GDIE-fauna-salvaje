b24254b9bd4b4f664b5f170567348001801ad528
Carlos Veny <carlosveny@yahoo.es>
2022-03-17 00:18:26 +0100

    Añadido echo de Success

--- .ignore/deploy.php
+++ .ignore/deploy.php
@@ -2,4 +2,5 @@
 shell_exec("rm -r repo");
 shell_exec("mkdir repo");
 shell_exec("git clone https://github.com/carlosveny/GDIE-fauna-salvaje.git /var/www/html/repo");
+echo "Success! Apache Server updated from github repo GDIE-fauna-salvaje.git\n";
 ?>
