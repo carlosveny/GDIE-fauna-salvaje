3a357f1d6d5891578358a59db03aad716e513ab8
Carlos Veny <carlosveny@yahoo.es>
2022-03-17 18:28:32 +0100

    .exe borrado

--- .ignore/deploy.php
+++ .ignore/deploy.php
@@ -1,6 +1,6 @@
 <?php
 shell_exec("rm -r repo");
 shell_exec("mkdir repo");
-shell_exec("git clone https://github.com/carlosveny/GDIE-fauna-salvaje.git /var/www/html/repo");
-echo "Success! Apache Server updated from github repo GDIE-fauna-salvaje.git\n";
+$output = shell_exec("git clone https://github.com/carlosveny/GDIE-fauna-salvaje.git /var/www/html/repo");
+echo $output;
 ?>
