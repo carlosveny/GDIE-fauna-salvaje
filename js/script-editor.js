9c8e50861475ef9d12a06bf8d50cc1e4c88f29cd
Carlos Veny <carlosveny@yahoo.es>
2022-03-23 23:01:43 +0100

    XAMPP support

--- js/script-editor.js
+++ js/script-editor.js
@@ -1,14 +1,53 @@
+/*
+    Fichero que gestiona la introduccion, modificacion y eliminacion
+    de metadatos en videos de animales y su posterior actualizacion
+    en el servidor.
+*/
+// https://www.w3schools.com/php/php_file_upload.asp (file uploads php)
 
-//plyr.setup("#player");
-
+// Funcion que se ejecuta al cargarse la pagina
 function loaded() {
-    const player = new Plyr('#player');
-
+    // Inicializacion del media player "plyr"
     const video = document.querySelector('#player');
-
     video.addEventListener('play', (event) => {
         console.log('The Boolean paused property is now false. Either the ' +
             'play() method was called or the autoplay attribute was toggled.');
     });
 
+    // Inicializacion del boton "Examinar"
+    const input = document.querySelector('#file-input');
+    input.addEventListener('input', subirVideo);
+
+}
+
+// Funcion que sube un video al servidor
+function subirVideo() {
+    var file = document.getElementById("file-input").files[0];
+    if (file == null) return;
+    //console.log(document.getElementById("file-input").files[0]);
+
+    // Pasar el archivo a formData
+    var formData = new FormData();
+    formData.append("file", file);
+
+    $.ajax({
+        url : "php/uploadVideo.php",
+        type: "POST",
+        data : formData,
+        processData: false,
+        contentType: false,
+        success:function(data, textStatus, jqXHR){
+            console.log(data);
+        },
+        error: function(jqXHR, textStatus, errorThrown){
+            //if fails
+        }
+    });
+
+    // $.post("localhost/FaunaSalvaje/php/uploadVideo.php", {
+    //     archivo: formData
+    // })
+    //     .done(function (data) {
+    //         console.log(data);
+    //     });
 }
---
+++ php/uploadVideo.php
@@ -0,0 +1,27 @@
+<?php
+
+if(isset($_FILES['file']['name'])){
+   // file name
+   $filename = $_FILES['file']['name'];
+
+   // Location
+   $location = '../assets/videos/'.$filename;
+
+   // file extension
+   $file_extension = pathinfo($location, PATHINFO_EXTENSION);
+   $file_extension = strtolower($file_extension);
+
+   // Valid extensions
+   $valid_ext = array("mp4","webm","ogg");
+
+   $response = 0;
+   if(in_array($file_extension,$valid_ext)){
+      // Upload file
+      if(move_uploaded_file($_FILES['file']['tmp_name'],$location)){
+         $response = 1;
+      }
+   }
+
+   echo $response;
+   exit;
+}
