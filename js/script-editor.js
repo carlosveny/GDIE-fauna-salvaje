e4ffe62d526bc7b694b07e9799f48572b55360ef
Carlos Veny <carlosveny@yahoo.es>
2022-03-23 12:55:59 +0100

    a

--- js/script-editor.js
+++ js/script-editor.js
@@ -3,4 +3,12 @@
 
 function loaded() {
     const player = new Plyr('#player');
+
+    const video = document.querySelector('#player');
+
+    video.addEventListener('play', (event) => {
+        console.log('The Boolean paused property is now false. Either the ' +
+            'play() method was called or the autoplay attribute was toggled.');
+    });
+
 }
