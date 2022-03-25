a93c08e06818e7dca156399e271c8b198f5ae151
Carlos Veny <carlosveny@yahoo.es>
2022-03-25 14:05:01 +0100

    editor actualizado

--- js/script-editor.js
+++ js/script-editor.js
@@ -18,40 +18,23 @@ function loaded() {
 
     // Inicializacion del boton "Examinar"
     const input = document.querySelector('#file-input');
-    input.addEventListener('input', subirVideo);
+    input.addEventListener('input', peticionSubirVideo);
 
-    cargarVideo("assets/animales.mp4");
+    // Inicializacion del dropdown "Video Existente"
+    peticionObtenerVideos();
+
+    //cargarVideo("assets/animales.mp4");
 }
 
-// Funcion que sube un video al servidor
-function subirVideo() {
-    var file = document.getElementById("file-input").files[0];
-    if (file == null) return;
-    //console.log(document.getElementById("file-input").files[0]);
-
-    // Pasar el archivo a formData
-    var formData = new FormData();
-    formData.append("file", file);
-
-    // Peticion POST al servidor para subir el archivo (si no existe)
-    $.ajax({
-        url: "php/uploadVideo.php",
-        type: "POST",
-        data: formData,
-        processData: false,
-        contentType: false,
-        success: function (data) {
-            var path = data.replace("../", "");
-            cargarVideo(path);
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            //if fails
-        }
-    });
-}
-
+// Funcion que carga un video dado su path
 function cargarVideo(path) {
-    console.log(path);
+    // Si es un objeto se ha elegido un video existente
+    if ((typeof path) == "object") {
+        path = path.value;
+        console.log(path);
+    }
+    
+    // Crear elemento "source"
     var src = document.createElement("source");
     setAttributes(src, { id: "video-src", src: path, type: "video/mp4" });
     document.getElementById("player").appendChild(src);
@@ -59,14 +42,19 @@ function cargarVideo(path) {
         document.getElementById("alerta-no-video").remove();
     }
 
+    // Deshabilitar la seleccion de nuevos videos
+    document.getElementById("file-input").disabled = true;
+    document.getElementById("file-selector").disabled = true;
+
 }
 
+// Funcion que lee los metadatos de un video y los rellena en la parte derecha
 function readDatos() {
     var videoElement = document.getElementById("player");
     var textTracks = videoElement.textTracks;
     var cues = textTracks[0].cues;
     var str = replaceAll(cues[0].text, "\n", "<br>");
-    document.getElementById("display-metadata").innerHTML = str;
+    //document.getElementById("display-metadata").innerHTML = str;
     console.log(cues[0]);
     //console.log(JSON.parse(cues[0].text));
 
@@ -94,7 +82,91 @@ function readDatos() {
     $('#md-esqueleto option[value=' + esqueleto + ']').attr('selected', "");
 }
 
+// Funcion que crea un aviso de bootstrap dado el tipo, titulo y descripcion
+// tipo: alert-danger, alert-warning, alert-success. (Todo de Bootstrap)
+function crearAviso(tipo, titulo, descr) {
+    // Crear aviso
+    var aviso = document.createElement("div");
+    aviso.classList.add("myAlert-top", "alert", "alert-dismissible", "fade" ,"show", tipo);
+    aviso.innerHTML = "<strong>" + titulo + " </strong>" + descr;
+    var cerrar = document.createElement("button");
+    cerrar.setAttribute("type", "button");
+    cerrar.classList.add("btn-close");
+    cerrar.setAttribute("data-bs-dismiss", "alert");
+    cerrar.setAttribute("aria-label", "Close");
+
+    // Append
+    aviso.appendChild(cerrar);
+    document.getElementById("cuerpo").appendChild(aviso);
+
+    // Mostrar y ocultar tras 3 segundos
+    $(".myAlert-top").show();
+    setTimeout(function(){
+        $(".myAlert-top").hide();
+      }, 3000);
+}
+
+/* ---------------------------------------------------------------------------- */
+
+// FUNCIONES POST Y GET
+
+// Funcion que sube un video al servidor
+function peticionSubirVideo() {
+    var file = document.getElementById("file-input").files[0];
+    if (file == null) return;
+    //console.log(document.getElementById("file-input").files[0]);
+
+    // Pasar el archivo a formData
+    var formData = new FormData();
+    formData.append("file", file);
+
+    // Peticion POST al servidor para subir el archivo (si no existe)
+    $.ajax({
+        url: "php/uploadVideo.php",
+        type: "POST",
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function (data) {
+            // Si ya existe un video con el mismo nombre mostrar aviso
+            if (data == "existe") {
+                var descr = "El vídeo seleccionado ya existe en el servidor. ";
+                descr = descr + "Selecciona otro vídeo o modifícale el nombre.";
+                crearAviso("alert-warning", "Aviso:", descr);
+            }
+            else {
+                var path = data.replace("../", "");
+                cargarVideo(path);
+            }
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            //if fails
+        }
+    });
+}
+
+// Funcion que solicita al servidor los paths de los videos existentes
+function peticionObtenerVideos() {
+    $.get("php/consultVideos.php", {})
+		.done(function (data) {
+            var paths = JSON.parse(data);
+            var select = document.getElementById("file-selector");
+            for (var i=0; i<paths.length; i++) {
+                var option = document.createElement("option");
+                option.setAttribute("value", paths[i]);
+                option.innerHTML = paths[i].replace("assets/videos/", "");
+                select.appendChild(option);
+            }
+
+            console.log(JSON.parse(data));
+            
+		});
+}
+
+/* ---------------------------------------------------------------------------- */
+
 // FUNCIONES AUXILIARES
+
 // Funcion auxiliar para añadir mas de 1 atributo a la vez (a un mismo elemento)
 // https://stackoverflow.com/questions/12274748/setting-multiple-attributes-for-an-element-at-once-with-javascript
 function setAttributes(el, attrs) {
---
+++ php/consultVideos.php
@@ -0,0 +1,23 @@
+<?php
+/*
+    Fichero que devuelve un array con todos los paths de los videos
+    que hay subidos en el servidor
+*/
+
+$relativePath = "../assets/videos/";
+$absolutePath = "assets/videos/";
+$files = scandir($relativePath);
+
+// Bucle para eliminar las posiciones que contienen '.' y '..' a causa de scandir
+// Tambien descarta los archivos que no son ".mp4"
+// Tambien concatena el path absoluto
+$archivos = [];
+for ($i = 0; $i < count($files); $i++) {
+    if (($files[$i] != ".") && ($files[$i] != "..") && (strpos($files[$i], '.mp4') !== false)) {
+        $archivos[] = $absolutePath . $files[$i];
+    }
+}
+
+// Devolver el array de paths
+// JSON_UNESCAPED_SLASHES (https://stackoverflow.com/questions/10210338/json-encode-escaping-forward-slashes)
+echo json_encode($archivos, JSON_UNESCAPED_SLASHES);
