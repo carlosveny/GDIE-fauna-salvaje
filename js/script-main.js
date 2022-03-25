bc318788f468136839f45a557444930d35173210
Marc Rossello <marc.rossello.es@gmail.com>
2022-03-25 15:24:25 +0100

    Index info dinámica

--- js/script-main.js
+++ js/script-main.js
@@ -7,10 +7,6 @@ function loaded() {
             'play() method was called or the autoplay attribute was toggled.');
     });
 
-    // Inicializacion del boton "Examinar"
-    const input = document.querySelector('#file-input');
-    input.addEventListener('input', subirVideo);
-
     cargarVideo("assets/animales.mp4");
 }
 
@@ -23,4 +19,53 @@ function cargarVideo(path) {
         document.getElementById("alerta-no-video").remove();
     }
 
+}
+
+
+function readDatos() {
+    var videoElement = document.getElementById("player");
+    var textTracks = videoElement.textTracks;
+    var cues = textTracks[0].cues;
+    console.log("aaaa");
+    console.log(JSON.parse(cues[0].text));
+
+    var info = JSON.parse(cues[0].text);
+    $("#nombreComun").text(info.nombreComun);
+    $("#nombreCientifico").text(info.nombreCientifico);
+    $("#descripcion").html(info.descripcion);
+
+    var continente = info.continente.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
+    var medio = info.medio.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
+    var alimentacion = info.alimentacion.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
+    var esqueleto = info.esqueleto.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
+
+    $("#alimentacion").text(info.alimentacion);
+    $("#iconoAlimentacion").attr("src", "assets/icons/"+ alimentacion +".ico" );
+    console.log(alimentacion);
+    $("#medio").text(info.medio);
+    $("#iconoMedio").attr("src", "assets/icons/"+ medio +".ico" );
+    $("#esqueleto").text(info.esqueleto);
+    $("#iconoEsqueleto").attr("src", "assets/icons/"+ esqueleto +".ico" );
+
+    /* $("#md-geoLat").attr("value", info.geoLat);
+    $("#md-geoLong").attr("value", info.geoLong);
+    $("#md-foto").attr("value", info.foto); */ 
+
+    
+
+}
+
+// FUNCIONES AUXILIARES
+// Funcion auxiliar para añadir mas de 1 atributo a la vez (a un mismo elemento)
+// https://stackoverflow.com/questions/12274748/setting-multiple-attributes-for-an-element-at-once-with-javascript
+function setAttributes(el, attrs) {
+    for (var key in attrs) {
+        el.setAttribute(key, attrs[key]);
+    }
+}
+
+// Funcion auxiliar que cambia todas las ocurrencias de una expresion en un string
+// https://stackoverflow.com/questions/1144783/how-to-replace-all-occurrences-of-a-string-in-javascript
+function replaceAll(str, find, replace) {
+    return str.replace(new RegExp(find, 'g'), replace);
 }
