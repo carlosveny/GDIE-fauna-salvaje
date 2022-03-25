7a32c0255ed944bd11291d3e1e74988d5c41475f
Carlos Veny <carlosveny@yahoo.es>
2022-03-25 21:53:54 +0100

    Cambios servidor

--- .server/deploy.php
+++ .server/deploy.php
@@ -1,6 +1,7 @@
 <?php
-shell_exec("rm -r repo");
-shell_exec("mkdir repo");
-shell_exec("git clone https://github.com/carlosveny/GDIE-fauna-salvaje.git /var/www/html/repo");
-echo "Success! Apache Server updated from github repo GDIE-fauna-salvaje.git\n";
+//shell_exec("rm -r repo");
+//shell_exec("mkdir repo");
+//shell_exec("git clone https://github.com/carlosveny/GDIE-fauna-salvaje.git /var/www/html/repo");
+$output = shell_exec("git pull origin main");
+echo $output;
 ?>
