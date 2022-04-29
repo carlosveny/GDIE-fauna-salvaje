91668d2048bbe5d5ff6ebaa18bb673c79a673ae7
Carlos Veny <carlosveny@yahoo.es>
2022-04-29 17:32:26 +0200

    Prueba webrtc

---
+++ js/script-directo.js
@@ -0,0 +1,35 @@
+const constraints = {'video': true, 'audio': true};
+
+function loaded() {
+    init();
+}
+
+async function init(e) {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    handleSuccess(stream);
+}
+
+function handleSuccess(stream) {
+    const video = document.querySelector('video');
+    const videoTracks = stream.getVideoTracks();
+    console.log('Got stream with constraints:', constraints);
+    console.log(`Using video device: ${videoTracks[0].label}`);
+    window.stream = stream; // make variable available to browser console
+    video.srcObject = stream;
+}
+
+function handleError(error) {
+    if (error.name === 'OverconstrainedError') {
+        const v = constraints.video;
+        errorMsg(`The resolution ${v.width.exact}x${v.height.exact} px is not supported by your device.`);
+    } else if (error.name === 'NotAllowedError') {
+        errorMsg('Permissions have not been granted to use your camera and ' +
+            'microphone, you need to allow the page access to your devices in ' +
+            'order for the demo to work.');
+    }
+    errorMsg(`getUserMedia error: ${error.name}`, error);
+}
+
+function errorMsg(msg, error) {
+    const errorElement = document.querySelector('#errorMsg');
+}
