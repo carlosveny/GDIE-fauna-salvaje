d3e2639ec09346d113f523752c9ebc0d19cca4f6
Carlos Veny <carlosveny@yahoo.es>
2022-05-01 16:29:48 +0200

    Node.js fixes

---
+++ node/server.js
@@ -0,0 +1,25 @@
+var WebSocketServer = require('ws').Server;
+var ws = new WebSocketServer({ port: 8080 });
+
+ws.on('listening', function () {
+	console.log("Server started with port 8080");
+});
+
+ws.on('connection', function (connection) {
+
+    console.log("User is connected");
+    connection.send("Hello from server");
+    
+    /* Action to do when user send messages */
+    connection.on('message', function (message) {
+        
+        console.log("Message from user");
+    });
+
+    
+    /* Action to do When user try to close the connection */
+    connection.on('close', function () {
+        console.log("Disconnecting user"); 
+    });  
+   
+});
