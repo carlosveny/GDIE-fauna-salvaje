44d1c46194a3dd6c5dba62cea8b1fd182f01f7b1
Carlos Veny <carlosveny@yahoo.es>
2022-05-01 21:44:17 +0200

    WebRTC avanzando

--- js/script-directo.js
+++ js/script-directo.js
@@ -5,9 +5,15 @@
 
 // VARIABLES GLOBALES
 var ws = new WebSocket("wss://alumnes-ltim.uib.es/gdie2208b/"); //open a web socket from javascript
+var username = null;
 
 function loaded() {
     playVideoFromCamera();
+}
+
+function setUsername() {
+    username = $("#username").val();
+    peticionUsuario(username);
 }
 
 async function playVideoFromCamera() {
@@ -15,34 +21,53 @@ async function playVideoFromCamera() {
         const constraints = {
             video: {
                 width: { ideal: 4096 },
-                height: { ideal: 2160 } 
+                height: { ideal: 2160 }
             },
             'audio': true
         };
         const stream = await navigator.mediaDevices.getUserMedia(constraints);
         const videoElement = document.querySelector('#localVideo');
         videoElement.srcObject = stream;
-    } catch(error) {
+    } catch (error) {
         console.error('Error opening video camera.', error);
     }
 }
 
-ws.onopen = function() {
-                  
-    // Web Socket is connected, send datas to server
-    ws.send("Message from user");
-    console.log("Message send to server");
- };
+/* ---------------------------------------------------------------------------- */
 
- ws.onmessage = function (evt) { 
+// FUNCIONES DE PETICIONES AL SIGNALING SERVER
 
-     // handle messages from server
+function peticionUsuario(user) {
+    var datos = {
+        "nombre": user,
+        "fecha": Date.now(),
+        "tipo": "usuario"
+    };
+    ws.send(JSON.stringify(datos));
+    console.log("Mensaje enviado al servidor");
+}
+
+
+/* ---------------------------------------------------------------------------- */
+
+// FUNCIONES QUE GESTIONAN LA CONEXION CON EL SIGNALING SERVER
+
+ws.onopen = function () {
+
+    // Web Socket is connected, send data to server
+    // ws.send("Message from user");
+    console.log("Conexión establecida");
+};
+
+ws.onmessage = function (evt) {
+
+    // handle messages from server
     var received_msg = evt.data;
-    alert("Mesage from server = "+received_msg);
- };
+    console.log("SERVIDOR: " + received_msg);
+};
 
- ws.onclose = function() { 
-                  
+ws.onclose = function () {
+
     // websocket is closed
     console.log("Websocket Connection is closed...");
- };
+};
